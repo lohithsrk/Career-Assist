82d47a715cb2e5d3e4865d17aae9155ccc4528d4
Prasanna RV <prasannavelmurugan0200@gmail.com>
2022-02-21 11:40:18 +0530

    rv

--- client/src/components/RegisterSocialComponent.jsx
+++ client/src/components/RegisterSocialComponent.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import { authentication } from "../firebase-config";
-
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 function RegisterSocialComponent() {
   const signInWithGoogle = () => {
-    signInWithPopup(authentication, provider);
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(authentication, provider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
     <div>
       <h1>RegisterSocialComponent</h1>
-      <button onClick={{ signInWithGoogle }}>Sign in with Google</button>
+      <button onClick={signInWithGoogle}>Sign in with Google</button>
     </div>
   );
 }
