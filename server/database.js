e792a501902305fd1929cea54dec1d154210a6d7
Lohith <srklohith05@gmail.com>
2022-02-22 09:06:39 +0530

    Changes

--- server/database.js
+++ server/database.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-	host: process.env.HOST,
+	host: process.env.QUESTIONS_HOST,
 	port: 3306,
-	database: process.env.DATABASE,
-	user: process.env.USER,
-	password: process.env.PASSWORD
+	database: process.env.QUESTIONS_DATABASE,
+	user: process.env.QUESTIONS_USER,
+	password: process.env.QUESTIONS_PASSWORD
 });
 
 module.exports = db;
