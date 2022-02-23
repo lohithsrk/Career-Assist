894e8f239b10b3a15d47cdb0d912f68000ba6fc7
Lohith <srklohith05@gmail.com>
2022-02-23 10:27:48 +0530

    Changes

--- server/controller/assessmentClient.controller.js
+++ server/controller/assessmentClient.controller.js
@@ -1,82 +1,98 @@
-const {db} = require('../database');
+const { questionsDB } = require('../database');
 
 const questionsClientGet = async (req, res) => {
 	const { extraSubject, jeeMark, boardMark } = req.query;
-	let category;
 
-	if (boardMark < 50) {
-		return res.json('You are not eligible for this test');
-	} else {
-		if (jeeMark >= 80) {
-			category = 'advanced';
-		} else if (jeeMark <= 80) {
-			category = 'basic';
+	class Marks {
+		constructor(jeeMark, boardMark) {
+			this.jeeMark = jeeMark;
+			this.boardMark = boardMark;
 		}
 	}
-	const physics = null;
-	await db.query(
-		`SELECT * FROM Physics WHERE category = ${category}`,
-		(request, responses) => {
-			responses &&
-				responses.forEach((response) => {
-					physics.push(response);
-				});
-		}
-	);
 
-	const chemistry = null;
-	await db.query(
-		`SELECT * FROM Chemistry WHERE category = ${category}`,
-		(request, responses) => {
-			responses &&
-				responses.forEach((response) => {
-					chemistry.push(response);
-				});
-		}
-	);
+	await questionsDB.query(`SELECT * FROM mark`, async (err, result) => {
+		const marks = new Marks(result[0].jee, result[0].board);
 
-	const maths = null;
-	await db.query(
-		`SELECT * FROM Maths WHERE category = ${category}`,
-		(request, responses) => {
-			responses &&
-				responses.forEach((response) => {
-					maths.push(response);
-				});
-		}
-	);
+		let category;
 
-	const biology = null;
-	await db.query(
-		`SELECT * FROM Biology WHERE category = ${category}`,
-		(request, responses) => {
-			responses &&
-				responses.forEach((response) => {
-					biology.push(response);
-				});
+		if (jeeMark >= marks.jee) {
+			category = 'advance';
+		} else {
+			if (boardMark >= marks.board) {
+				category = 'advance';
+			} else {
+				category = 'basic';
+			}
 		}
-	);
+		const physics = null;
+		await questionsDB.query(
+			`SELECT * FROM Physics WHERE category = ${category}`,
+			(request, responses) => {
+				responses &&
+					responses.forEach((response) => {
+						physics.push(response);
+					});
+			}
+		);
 
-	const computer = null;
-	await db.query(
-		`SELECT * FROM Computer WHERE category = ${category}`,
-		(request, responses) => {
-			responses &&
-				responses.forEach((response) => {
-					computer.push(response);
-				});
+		const chemistry = null;
+		await questionsDB.query(
+			`SELECT * FROM Chemistry WHERE category = ${category}`,
+			(request, responses) => {
+				responses &&
+					responses.forEach((response) => {
+						chemistry.push(response);
+					});
+			}
+		);
+
+		const maths = null;
+		await questionsDB.query(
+			`SELECT * FROM Maths WHERE category = ${category}`,
+			(request, responses) => {
+				responses &&
+					responses.forEach((response) => {
+						maths.push(response);
+					});
+			}
+		);
+
+		const biology = null;
+		await questionsDB.query(
+			`SELECT * FROM Biology WHERE category = ${category}`,
+			(request, responses) => {
+				responses &&
+					responses.forEach((response) => {
+						biology.push(response);
+					});
+			}
+		);
+
+		const computer = null;
+		await questionsDB.query(
+			`SELECT * FROM Computer WHERE category = ${category}`,
+			(request, responses) => {
+				responses &&
+					responses.forEach((response) => {
+						computer.push(response);
+					});
+			}
+		);
+		if (physics >= 15 && chemistry >= 15 && maths >= 15) {
+			res.json([
+				physics,
+				chemistry,
+				maths,
+				extraSubject == 'biology' ? biology : computer
+			]);
+		} else {
+			res.json('Not enough questions');
 		}
-	);
-	if (physics >= 15 && chemistry >= 15 && maths >= 15) {
-		res.json([
-			physics,
-			chemistry,
-			maths,
-			extraSubject == 'biology' ? biology : computer
-		]);
-	} else {
-		res.json('Not enough questions');
-	}
+	});
 };
 
-module.exports = { questionsClientGet };
+const questionsClientPost = async (req, res) => {
+	
+};
+
+module.exports = { questionsClientGet, questionsClientPost };
